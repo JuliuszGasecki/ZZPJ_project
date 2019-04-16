69be4c2b04a941abd4ad4dfc87e92fca74a740ce
Juliusz Gąsecki <julekg.97@gmail.com>
2019-04-16 21:56:50 +0200

    Project init commit

---
+++ src/main/java/pl/javowe/swirki/zzpjapp/ZzpjAppApplication.java
@@ -0,0 +1,13 @@
+package pl.javowe.swirki.zzpjapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzpjAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzpjAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/javowe/swirki/zzpjapp/ZzpjAppApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.javowe.swirki.zzpjapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZzpjAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
