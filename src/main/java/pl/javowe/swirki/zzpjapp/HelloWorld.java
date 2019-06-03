876861f59cf9af158b9116e223384f9c429fe130
Juliusz Gąsecki <julekg.97@gmail.com>
2019-06-03 18:33:46 +0200

    Spring Security basic config

--- src/main/java/pl/javowe/swirki/zzpjapp/HelloWorld.java
+++ src/main/java/pl/javowe/swirki/zzpjapp/HelloWorld.java
@@ -11,6 +11,18 @@ import java.io.IOException;
 public class HelloWorld {
 
     @RequestMapping("/hello")
+    public String hello(){
+        CreatorCV creatorCV = new CreatorCV();
+        try {
+            creatorCV.createCV(new User());
+        } catch (IOException e) {
+            System.out.println("lol");
+            e.printStackTrace();
+        }
+        return "HelloWorld";
+    }
+
+    @RequestMapping("/")
     public String index(){
         CreatorCV creatorCV = new CreatorCV();
         try {
---
+++ src/main/java/pl/javowe/swirki/zzpjapp/security/SpringSecurityConfig.java
@@ -0,0 +1,61 @@
+package pl.javowe.swirki.zzpjapp.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/").permitAll()
+                .antMatchers("/css/**", "/js/**", "fonts/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+/*
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+ */
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                users
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build());
+        manager.createUser(
+                users
+                        .username("root")
+                        .password("toor")
+                        .roles("ADMIN")
+                        .build());
+        return manager;
+    }
+
+}
