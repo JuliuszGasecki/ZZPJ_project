f2847018a9224a057fac17dd2c73fc3279858b80
Adrianna <adriannabachulska@gmail.com>
2019-06-25 00:12:07 +0200

    saving user avatar to db

--- src/main/java/pl/javowe/swirki/zzpjapp/model/ChatMessage.java
+++ src/main/java/pl/javowe/swirki/zzpjapp/model/ChatMessage.java
@@ -1,31 +1,9 @@
 package pl.javowe.swirki.zzpjapp.model;
 
-import lombok.Data;
-
-import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotEmpty;
-
-@Data
-@Entity
-@Table(name = "chat_message")
 public class ChatMessage {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable =  false)
     private Long id;
-
-    @Column(name = "message_type", nullable = false)
     private MessageType type;
-
-    @Column(name = "content", nullable = false)
-    @NotEmpty
-    @Max(500)
     private String content;
-
-    @Column(name = "sender", nullable = false)
-    @NotEmpty
-    @Max(50)
     private String sender;
 
     public ChatMessage()
